e40b9c00ce2cf93eb00931eee4bfcb4abe6d2349
ttalVlatt <97192696+ttalVlatt@users.noreply.github.com>
2023-06-19 00:37:25 +0100

    Got static conditional change working

--- _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
+++ _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
@@ -1,19 +1,25 @@
 -- Converts default grey horiztonal rule to thin black line (customizable in line 10)
 -- h/t https://github.com/jgm/pandoc/issues/2573
 -- h/t https://gist.github.com/Merovex/05e3216f8f4f6e965cd9d564b1496719
-local horizontallinerule = [[<w:p>
+
+local color = "00FF00"
+local style = "double"
+
+local horizontallinerule = string.format([[<w:p>
   <w:pPr>
     <w:pStyle w:val="HorizontalRule"/>
       <w:ind w:firstLine="0"/>
       <w:jc w:val="center"/>
     <w:pBdr>
-      <w:bottom w:val="single" w:color="000000"/>
+      <w:bottom w:val="%s" w:color="%s"/>
     </w:pBdr>
   </w:pPr>
   <w:r>
     <w:t></w:t>
   </w:r>
-</w:p>]]
+</w:p>]], style, color)
+
+
 function HorizontalRule (elem)
     if FORMAT == 'docx' then
       return pandoc.RawBlock('openxml', horizontallinerule)
