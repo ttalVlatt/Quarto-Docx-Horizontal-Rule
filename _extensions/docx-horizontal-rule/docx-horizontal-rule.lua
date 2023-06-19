eb3533aa5934694a1f1fa48089eacb764a99ac9b
ttalVlatt <97192696+ttalVlatt@users.noreply.github.com>
2023-06-19 03:07:12 +0100

    Stuck on bringing in YAML data

--- _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
+++ _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
@@ -2,26 +2,26 @@
 -- h/t https://github.com/jgm/pandoc/issues/2573
 -- h/t https://gist.github.com/Merovex/05e3216f8f4f6e965cd9d564b1496719
 
-local color = "00FF00"
-local style = "double"
-
-local horizontallinerule = string.format([[<w:p>
-  <w:pPr>
-    <w:pStyle w:val="HorizontalRule"/>
-      <w:ind w:firstLine="0"/>
-      <w:jc w:val="center"/>
-    <w:pBdr>
-      <w:bottom w:val="%s" w:color="%s"/>
-    </w:pBdr>
-  </w:pPr>
-  <w:r>
-    <w:t></w:t>
-  </w:r>
-</w:p>]], style, color)
-
-
 function HorizontalRule (elem)
     if FORMAT == 'docx' then
+    
+    local color = "0000FF"
+    local style = "double"
+
+    local horizontallinerule = string.format([[<w:p>
+      <w:pPr>
+        <w:pStyle w:val="HorizontalRule"/>
+          <w:ind w:firstLine="0"/>
+          <w:jc w:val="center"/>
+        <w:pBdr>
+          <w:bottom w:val="%s" w:color="%s"/>
+        </w:pBdr>
+      </w:pPr>
+      <w:r>
+        <w:t></w:t>
+      </w:r>
+    </w:p>]], style, color)
+
       return pandoc.RawBlock('openxml', horizontallinerule)
     end
 end
