a6fc4de4ceb901cefaafd9acc4c94ee1c7b3ca10
ttalVlatt <97192696+ttalVlatt@users.noreply.github.com>
2023-06-20 00:13:04 +0100

    Finally working with YAML color input!

--- _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
+++ _extensions/docx-horizontal-rule/docx-horizontal-rule.lua
@@ -2,26 +2,38 @@
 -- h/t https://github.com/jgm/pandoc/issues/2573
 -- h/t https://gist.github.com/Merovex/05e3216f8f4f6e965cd9d564b1496719
 
-function HorizontalRule (elem)
-    if FORMAT == 'docx' then
+local function change_color (color_picked)
+  
+  return function (el)
     
-    local color = "0000FF"
-    local style = "double"
-
-    local horizontallinerule = string.format([[<w:p>
+    if color_picked then
+      
+     local color = pandoc.utils.stringify(color_picked) or "FF0000"
+      
+     local horizontallinerule = string.format([[<w:p>
       <w:pPr>
         <w:pStyle w:val="HorizontalRule"/>
           <w:ind w:firstLine="0"/>
           <w:jc w:val="center"/>
         <w:pBdr>
-          <w:bottom w:val="%s" w:color="%s"/>
+          <w:bottom w:val="triple" w:color="%s"/>
         </w:pBdr>
       </w:pPr>
       <w:r>
         <w:t></w:t>
       </w:r>
-    </w:p>]], style, color)
+    </w:p>]], color)
 
-      return pandoc.RawBlock('openxml', horizontallinerule)
+      return {
+        pandoc.RawBlock('openxml', horizontallinerule)
+      }
     end
+  end
+end
+
+function Pandoc (doc)
+  local color_choice = doc.meta['docxhrcolor']
+  return doc:walk {
+    HorizontalRule = change_color(color_choice),
+  }
 end
